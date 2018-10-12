e879725c62656f4b53ae131d612b703cb0f43542
Joseph Chingalo <profschingalo@gmail.com>
2018-10-12 16:52:37 +0300

    Adding smaple single page

--- server.js
+++ server.js
@@ -2,12 +2,29 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // support encoded bodies
+
 //adding static folder
 app.use(`/static`, express.static(`static`));
 
 // adding routes
 app.get('/', function (req, res) {
     res.sendFile(path.join(`${__dirname}/pages/home.html`));
+});
+
+// app.post('/send-email', function (req, res) {
+//     console.log(JSON.stringify({
+//         req,
+//         res
+//     }))
+// });
+
+app.post('/send-email', function (req, res) {
+    console.log("here")
 });
 
 app.listen(3001);
