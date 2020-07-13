488c33558c3b630c94e35cafd32d19775aa9e7a8
Joseph Chingalo <profschinfalo@gmail.com>
2020-07-13 12:19:25 +0300

    chore: remove some codes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/) 
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0) [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
 
 ## Prerequisites
 
@@ -8,8 +8,9 @@
 
 ## How to get stated with installation
 
+Clone the respoistory then run below command
+
 ```bash
-$ git clone https://github.com/chingalo/server-home-page.git
-$ cd server-home-page
+
 $ npm i
 ```
--- server.js
+++ server.js
@@ -1,59 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const _ = require('lodash')
-const appConfig = require('./config');
+
+const bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 //adding static folder
 app.use(`/static`, express.static(`static`));
 
 // adding routes
-app.get('/', function(req, res) {
-    res.sendFile(path.join(`${__dirname}/pages/index.html`));
-});
-app.post('/send-email', function(req, res) {
-    const {
-        subject,
-        _replyto,
-        name,
-        message
-    } = req.body;
-    try {
-        const emailConfig = appConfig.emailConfig || {}
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            requireTLS: true,
-            auth: {
-                user: emailConfig.username || "",
-                pass: emailConfig.password || ""
-            }
-        });
-        const mailOptions = {
-            to: _.join(appConfig.users || [], ","),
-            subject: `${name} : ${subject} `,
-            html: `<b>Name : </b>${name} <br><b>E-mail : </b>${_replyto}<br><b>Message : </b>${message}`
-        }
-        console.log(JSON.stringify({ mailOptions }))
-        transporter.sendMail(mailOptions, function(error, info) {
-            if (error) {
-                console.log(JSON.stringify({ error }));
-            } else {
-                console.log(JSON.stringify({ info }));
-            }
-            res.redirect('/');
-        })
-    } catch (error) {
-        console.log({ error })
-        res.redirect('/');
-    }
+app.get("/", function (req, res) {
+  res.sendFile(path.join(`${__dirname}/pages/index.html`));
 });
 
 app.listen(3001);
